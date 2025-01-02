aeb85af0a9e059a6efa14749b0fb76021d0ed86f
Ahmet Eris <ahmeteris81@hotmail.com>
2025-01-03 02:12:16 +0300

    Update sync.js

--- sync.js
+++ sync.js
@@ -9,17 +9,21 @@ const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY})
 // SOAP request configuration
 const SOAP_URL = 'https://www.nemodabutik.com/Servis/UrunServis.svc?wsdl';
 const SOAP_BODY = `
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:ns="http://schemas.datacontract.org/2004/07/">
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:ns="http://schemas.datacontract.org/2004/07/" xmlns:arr="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
    <soapenv:Header/>
    <soapenv:Body>
       <tem:SelectUrun>
          <tem:UyeKodu>X0RIYB6FC9SOT8TH9YWT7TGL6JMLU4</tem:UyeKodu>
          <tem:f>
             <ns:Aktif>-1</ns:Aktif>
+            <ns:EntegrasyonDegerTanim></ns:EntegrasyonDegerTanim>
+            <ns:EntegrasyonKodu></ns:EntegrasyonKodu>
          </tem:f>
          <tem:s>
             <ns:BaslangicIndex>0</ns:BaslangicIndex>
-            <ns:KayitSayisi>200</ns:KayitSayisi>
+            <ns:KayitSayisi>3</ns:KayitSayisi>
+            <ns:SiralamaDegeri>id</ns:SiralamaDegeri>
+            <ns:SiralamaYonu>Desc</ns:SiralamaYonu>
          </tem:s>
       </tem:SelectUrun>
    </soapenv:Body>
@@ -30,31 +34,39 @@ async function syncProducts() {
         // Ticimax'den veri çek
         const response = await axios.post(SOAP_URL, SOAP_BODY, {
             headers: {
-                'Content-Type': 'text/xml;charset=UTF-8',
-                'SOAPAction': 'http://tempuri.org/SelectUrun'
+                'Content-Type': 'text/xml; charset=utf-8',
+                'SOAPAction': 'http://tempuri.org/IUrunServis/SelectUrun'
             }
         });
 
         // XML'i parse et
-        const parser = new xml2js.Parser();
+        const parser = new xml2js.Parser({explicitArray: false});
         const result = await parser.parseStringPromise(response.data);
 
+        // Debug için response'u logla
+        console.log('API Response:', JSON.stringify(result, null, 2));
+
         // Ürün listesini al
-        const products = result.Envelope.Body[0].SelectUrunResponse[0].SelectUrunResult[0].UrunKarti;
+        const products = result['s:Envelope']['s:Body'].SelectUrunResponse.SelectUrunResult.a_UrunKarti;
 
         // Her ürün için Airtable'a kayıt ekle
         for (const product of products) {
-            await base('Ürün Listesi').create({
-                "Ürün KartID": product.ID[0],
-                "Ürün Adı": product.UrunAdi[0],
-                "Fiyatı": product.Varyasyonlar[0].SatisFiyati[0],
-                "URL": product.URL[0]
-            });
+            try {
+                await base('Ürün Listesi').create({
+                    "Ürün KartID": product.a_ID,
+                    "Ürün Adı": product.a_UrunAdi,
+                    "Fiyatı": product.a_Varyasyonlar?.a_SatisFiyati || "",
+                    "URL": product.a_URL || ""
+                });
+                console.log(`Ürün eklendi: ${product.a_UrunAdi}`);
+            } catch (err) {
+                console.error(`Ürün eklenirken hata: ${err.message}`);
+            }
         }
 
-        console.log('Sync completed successfully!');
+        console.log('Senkronizasyon tamamlandı!');
     } catch (error) {
-        console.error('Error during sync:', error);
+        console.error('Senkronizasyon hatası:', error.response?.data || error.message);
         process.exit(1);
     }
 }
